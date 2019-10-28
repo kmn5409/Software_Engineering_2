bdbaec22e3b2d53beba037f16994acd50d6d6f4e
fallingfractal <rahel.t.stewart@gmail.com>
2019-10-28 09:35:26 -0400

    href

--- School_Logger/src/app/app-routing.module.ts
+++ School_Logger/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   
   {path : '', loadChildren: () => import('./parent/tabs/tabs.module').then(m => m.TabsPageModule)},
 
-  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) },
+  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) }
 
   //{ path: 'parent/overview/:id', loadChildren: () => import('./parent/parent-overview/parent-overview.module').then(m => m.ParentOverviewPageModule) }
 ];
