9e022a4338e606e9db38dff76fbce14cd95c4f99
tonoxic <loco_monkey_7@hotmail.com>
2019-11-06 15:57:46 -0400

    Fixed conflicts

--- School_Logger/src/app/teacher/students/students.page.ts
+++ School_Logger/src/app/teacher/students/students.page.ts
@@ -12,17 +12,10 @@ export class StudentsPage implements OnInit {
   childrenRef: AngularFireList<any>;
   children: Observable<any[]>;
 
-<<<<<<< HEAD
-  constructor(public db: AngularFireDatabase) {
-  this.tasksRef = db.list('/tasks/users');
-
-  this.tasks = this.tasksRef.snapshotChanges().pipe(
-=======
   constructor(public db: AngularFireDatabase){
   this.childrenRef = db.list('/children');
 
     this.children = this.childrenRef.snapshotChanges().pipe(
->>>>>>> b54666fd7be693572192a0d8fbf04160dd9a37e3
       map(changes =>
         changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
       )
