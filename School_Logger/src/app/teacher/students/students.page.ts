93dbf3cc1c49212bf622fa08f34755d088d82a47
kmn5409 <nichols.keanu9@gmail.com>
2019-11-06 09:48:58 -0400

    Added Firebase link

--- School_Logger/src/app/teacher/students/students.page.ts
+++ School_Logger/src/app/teacher/students/students.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-students',
@@ -6,8 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.page.scss'],
 })
 export class StudentsPage implements OnInit {
+  tasksRef: AngularFireList<any>;
+  tasks: Observable<any[]>;
 
-  constructor() { }
+  constructor(public db: AngularFireDatabase){
+  this.tasksRef = db.list('/tasks/users');
+
+    this.tasks = this.tasksRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+
+  }  
 
   ngOnInit() {
   }
--- School_Logger/src/environments/environment.ts
+++ School_Logger/src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB_tX2r0L4i-ZFf4UD-3Ln--FKK5_KXmVg',
+    projectId: 'se-project-4c4c4',
+    databaseURL: 'https://se-project-4c4c4.firebaseio.com/'
+  }
+
 };
 
 /*
