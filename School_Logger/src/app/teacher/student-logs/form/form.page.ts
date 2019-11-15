4a209b2fb9caef2ce89d7e08412dfa3e8c954a71
kmn5409 <nichols.keanu9@gmail.com>
2019-11-15 18:03:22 -0400

    Added pushing logs

--- School_Logger/src/app/teacher/student-logs/form/form.page.ts
+++ School_Logger/src/app/teacher/student-logs/form/form.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map, first } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+interface Log{
+    details: string;
+}
 
 @Component({
   selector: 'app-form',
@@ -6,9 +15,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.page.scss'],
 })
 export class FormPage implements OnInit {
+childrenCollection: AngularFirestoreCollection<any>;
+children: Observable<any[]>;
 
-  constructor() { }
+  constructor(private db: AngularFirestore, public af: AngularFireAuth) { }
 
+  capture_Data(){
+    console.log(this.data);
+    this.childrenCollection =  this.db.collection('logs');
+    this.childrenCollection.add({logDetails: this.data});
+
+  }
+    
   ngOnInit() {
   }
 
