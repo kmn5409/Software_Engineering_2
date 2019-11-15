5d248b43dde62e716198f28fdd0e645672e87bd9
kmn5409 <nichols.keanu9@gmail.com>
2019-11-15 13:44:10 -0400

    Fixed function names

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 import unittest
-import calculator
+import school 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 
 class Test(unittest.TestCase):
   def test_add(self):
-    self.assertEqual(calculator.add(2, 3), True)
+    cred = credentials.Certificate('se-project-4c4c4-7051f39dca6d.json')
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+
+    users_ref = db.collection(u'users')
+    users = users_ref.stream()
+    students = db.collection(u'children')
+    students = students.stream()
+
+    self.assertEqual(school.check_all_children_have_teachers(students), True)
     
 if __name__ == '__main__':
   unittest.main()
