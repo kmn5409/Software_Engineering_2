04039568102b08471e33c6f9be8bf22d617dc774
kmn5409 <nichols.keanu9@gmail.com>
2019-11-15 12:29:10 -0400

    Added CloudFirestore Pulls the data from firebase using our updated Cloud Firestore database

--- School_Logger/src/app/teacher/students/students.page.ts
+++ School_Logger/src/app/teacher/students/students.page.ts
@@ -1,29 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, first } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.page.html',
   styleUrls: ['./students.page.scss'],
 })
-export class StudentsPage implements OnInit {
-  childrenRef: AngularFireList<any>;
+export class StudentsPage {
+childrenCollection: AngularFirestoreCollection<any>;
   children: Observable<any[]>;
-
-  constructor(public db: AngularFireDatabase) {
-  this.childrenRef = db.list('/children');
-
-  this.children = this.childrenRef.snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
-      )
-    );
-
-  }
+  constructor(private db: AngularFirestore, public af: AngularFireAuth){}
 
   ngOnInit() {
+    this.childrenCollection =  this.db.collection('children');
+    this.children = this.childrenCollection.valueChanges();
+    //this.af.auth.subscribe(auth => console.log(auth));
+    console.log(this.af.auth);
+    console.log('User');
+    console.log(this.af.auth.currentUser);
+    console.log(this.af.auth.currentUser.email);
   }
 
+
 }
+
