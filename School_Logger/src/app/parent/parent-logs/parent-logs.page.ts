7a20bae006c45daa4de88de9b0e38a392e7062b2
fallingfractal <rahel.t.stewart@gmail.com>
2019-11-23 16:31:38 -0400

    showing logs

--- School_Logger/src/app/parent/parent-logs/parent-logs.page.ts
+++ School_Logger/src/app/parent/parent-logs/parent-logs.page.ts
@@ -11,10 +11,39 @@ import { Observable } from 'rxjs';
 })
 export class ParentLogsPage implements OnInit {
   id;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private afAuth: AngularFireAuth) { }
 
+  
+
+  logData = new Observable((observer) => {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        const db = firestore();
+        const logref = db.collection('logs');
+        const query = logref.where('childID' , '==', this.id).get()
+        .then(snapshot => {
+          if (snapshot.empty) {
+            console.log('No matching documents.');
+            return;
+          }  
+          let x = [];
+          snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            x.push(doc.data());
+           
+          });
+          observer.next(x);
+          
+        })
+        .catch(err => {
+          console.log('Error getting documents', err);
+        });
+      }
+    })
+  })
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
+    
   }
 
 }
